0a3f5a81bf5b7263d0fbcf426b811d4183310a3b
Dimi <dimi.o@hotmail.com>
2022-12-26 08:56:22 +0100

    Add project files.

---
+++ MauiBuildConfigurationBug/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiBuildConfigurationBug;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MauiBuildConfigurationBug/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace MauiBuildConfigurationBug;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
